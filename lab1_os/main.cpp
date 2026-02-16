24d572fce19d13a34da41a2b899068e4996e3526
luvlizzie <kudinovaea2006@yandex.by>
2026-02-16 18:48:58 +0300

    Initial Commit

---
+++ lab1_os/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab1_os
+//
+//  Created by Елизавета on 16.02.26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
